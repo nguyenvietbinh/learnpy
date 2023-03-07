dbd9b6f8c2355d0dada539470b7ab30f8848099b
nguyenvietbinh <126882221+nguyenvietbinh@users.noreply.github.com>
2023-03-07 15:55:19 +0000

    taoscreen

---
+++ screen.py
@@ -0,0 +1,9 @@
+import pygame
+pygame.init()
+screen = pygame.display.set_mode((800,900))
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+    pygame.display.update()
